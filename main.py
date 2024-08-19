f5b94d0fecf328cac65e48006cd69a0df64243c7
Anonymous <136169348+LPopivnyak@users.noreply.github.com>
2024-08-19 15:02:10 +0300

    777

--- main.py
+++ main.py
@@ -4,36 +4,43 @@ from PyQt6.QtWidgets import*
 app = QApplication([])
 window = QWidget()
 
-window = QWidget()
+
+
 text_edit = QTextEdit()
+list_notes_lbl = QLineEdit()
 notes_list = QListWidget()
 create_note_btn = QPushButton()
-save_note_btn = QPushButton()
 delete_note_btn = QPushButton()
-add_tag_btn = QPushButton()
-list_notes_lbl = QLineEdit()
-tag_list = QListWidget()
-tag_input = QLineEdit()
+save_note_btn = QPushButton()
+list_teg_lbl = QLineEdit()
+tegs_list = QListWidget()
+teg_input = QLineEdit()
+add_teg_btn = QPushButton()
 delete_teg_btn = QPushButton()
 search_note_btn = QPushButton()
+
+
 
 main_line = QHBoxLayout()
 main_line.addWidget(text_edit)
 
-
 v1 = QVBoxLayout()
 v1.addWidget(list_notes_lbl)
 v1.addWidget(notes_list)
-v1.addWidget(add_tag_btn)
-v1.addWidget(delete_teg_btn)
-v1.addWidget(tag_list)
-v1.addWidget(tag_input)
-v1.addWidget(search_note_btn)
-v1.addWidget(delete_note_btn)
-v1.addWidget(text_edit)
+h1 = QHBoxLayout()
+h1.addWidget(create_note_btn)
+h1.addWidget(delete_teg_btn)
 v1.addWidget(save_note_btn)
-v1.addWidget(create_note_btn)
+v1.addWidget(list_teg_lbl)
+v1.addWidget(tegs_list)
+h2 = QHBoxLayout()
+h2.addWidget(add_teg_btn)
+h2.addWidget(delete_teg_btn)
+v1.addWidget(delete_teg_btn)
+v1.addWidget(search_note_btn)
+
 main_line.addLayout(v1)
+
 
 
 window.setLayout(main_line)
